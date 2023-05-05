d3246e05b044ac2a2ba984993c449e39c94ee2ea
Shahzaib Naeem <shahzaibna786@gmail.com>
2023-05-05 16:41:19 +0500

    Error page added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <>
+      <Wrapper>
+        <div>
+          <h1>404</h1>
+          <h3>sorry, the page you tried cannot be found</h3>
+          <Link className="btn" to="/">
+            Back Home
+          </Link>
+        </div>
+      </Wrapper>
+    </>
+  );
 };
+
 const Wrapper = styled.section`
-  min-height: 100vh;
   display: grid;
   place-items: center;
   background: var(--clr-primary-10);
+  min-height: 100vh;
   text-align: center;
   h1 {
     font-size: 10rem;
